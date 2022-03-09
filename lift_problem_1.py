f181a8c291485785ea0d6aaee3f854d70dce61c8
mehar-srinivas-chowdari <84114918+mehar-srinivas-chowdari@users.noreply.github.com>
2022-03-10 00:02:13 +0530

    Update lift_problem_1.py

--- lift_problem_1.py
+++ lift_problem_1.py
@@ -1,1 +1,13 @@
-n=int(input())
+N=int(input())  # max number in lift 
+W=int(input()) # max weight of lift 
+num=int(input()) # people waiting out
+li=list(map(int,input().split())) # list of waiting people
+li.sort()
+x=0
+cnt=0
+for i in li:
+  if(x>=W):
+    break
+  x+=i
+  cnt+=1
+print(cnt)
