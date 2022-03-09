25d521570bb7ce084634208569674aeee5cf984b
mehar-srinivas-chowdari <84114918+mehar-srinivas-chowdari@users.noreply.github.com>
2022-03-10 00:23:31 +0530

    Update lift_problem_1.py

--- lift_problem_1.py
+++ lift_problem_1.py
@@ -1,19 +1,26 @@
-N=int(input())  # max number in lift 
-W=int(input()) # max weight of lift 
-num=int(input()) # people waiting out
-li=list(map(int,input().split())) # list of waiting people
+N,W=map(int,input().split())
+num=int(input())
+li=list(map(int,input().split()))
 li.sort()
 x=0
 cnt=0
 for i in li:
-  if(x>=W or cnt>=N):
-    break
-  x+=i
-  cnt+=1
+    x+=i
+    cnt+=1
+    if(x>=W or cnt>=N):
+        break
 print(cnt)
+
 '''
 10
 680
 15
 67 54 33 59 60 72 48 23 56 10 14 32 40 20 80
+10
+'''
+'''
+6 120
+10
+23 45 18 22 10 14 67 30 24 8
+6
 '''
